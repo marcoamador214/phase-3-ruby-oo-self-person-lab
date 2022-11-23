80d901beda8c053bc1aeb9c04fe1b8111059c8eb
Marco Amador <marcoamador214@gmail.com>
2022-11-23 09:19:42 -0800

    commit tree e907968107d513a71a70c58c35fd538b518b18a8

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,15 @@
 # your code goes here
+require 'pry'
+
+class Person
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+end
+
+# binding.pry
